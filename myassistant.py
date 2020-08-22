d42737b58e32e39e6313f1d720ef17ba74a5128c
kajal2325 <64985813+kajal2325@users.noreply.github.com>
2020-08-22 23:46:30 +0530

    Update myassistant.py

--- myassistant.py
+++ myassistant.py
@@ -9,27 +9,21 @@ while True:
     p=input()
     if("dont" in p):
         print("cant open application")    
-    elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("chrome" in p):
+    elif (("run" in p) or ("execute" in p) or ("open" in p)) and (("chrome" in p) or ("browser" in p)):
         pyttsx3.speak("yes, here is chrome")
         os.system("chrome")
     elif (("run" in p) or ("execute" in p) or ("open" in p)) and (("notepad" in p) or ("editor" in p)):
         pyttsx3.speak("yes, here is notepad")
         os.system("notepad")
-    elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("vlc" in p):
+    elif (("run" in p) or ("play" in p) or ("open" in p)) and ("vlc" in p):
         pyttsx3.speak("yes, here is vlc")
         os.system("vlc")
-    elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("calendar" in p):
-        pyttsx3.speak("yes, here is calendar")
-        os.system("calendar")
-    elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("calculator" in p):
-        pyttsx3.speak("yes, here is calculator")
-        os.system("calculator")
-    elif (("run" in p) or ("execute" in p) or ("open" in p)) and (("player" in p) or ("media" in p)):
+    elif (("run" in p) or ("play" in p) or ("open" in p)) and (("player" in p) or ("media" in p)):
         pyttsx3.speak("yes, here is wmplayer")
         os.system("wmplayer")
     elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("powerpoint" in p):
         pyttsx3.speak("yes, here is PowerPoint")
-        os.system("PowerPoint")
+        os.system("POWERPNT")
     elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("Control Panel" in p):
         pyttsx3.speak("yes, here is Control Panel")
         os.system("Control Panel")
@@ -39,15 +33,24 @@ while True:
     elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("Command Prompt" in p): 
         pyttsx3.speak("yes, here is Command Prompt")
         os.system("Command Prompt")
-   elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("Discord" in p): 
+    elif (("run" in p) or ("execute" in p) or ("open" in p)) and (("calc" in p) or ("calculator" in p)): 
+        pyttsx3.speak("yes, here is calc")
+        os.system("calc")
+    elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("OUTLOOK" in p): 
+        pyttsx3.speak("yes, here is OUTLOOK")
+        os.system("OUTLOOK")
+    elif (("run" in p) or ("get" in p) or ("open" in p)) and ("IP" in p): 
+        pyttsx3.speak("yes, here is IP")
+        os.system("ipconfig")
+    elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("Discord" in p): 
         pyttsx3.speak("yes, here is Discord")
         os.system("Discord")
     elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("Excel" in p): 
         pyttsx3.speak("yes, here is Excel")
         os.system("Excel")
-    elif (("run" in p) or ("execute" in p) or ("open" in p)) and ("Word" in p): 
+    elif (("run" in p) or ("launch" in p) or ("open" in p)) and ("Word" in p): 
         pyttsx3.speak("yes, here is Word")
-        os.system("Word")
+        os.system("WINWORD")
     elif (("run" in p) or ("launch" in p) or ("open" in p) ) and (("paint" in p)):
         pyttsx3.speak("yes, here is paint")
         os.system("paint")
